a3efed451af14e264fb30e08b90d6b063cf227c0
Amonsta <amana.ammi@gmail.com>
2021-07-08 14:09:05 -0600

    initial commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,30 @@
+// Values And Variables Assessment
+// let country = "United States";
+// let continent = "North America";
+// let population = 331002651;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data Types
+// let isIsland = false;
+// let language;
+
+// console.log(isIsland); // returns false
+// console.log(language); // returns undefined
+
+// let, cons, var
+
+// language = "English"
+// const isIsland = false
+// let country = "United States"
+
+// console.log(isIsland); 
+// console.log(language);
+
+// Basic Operators
+
+
+
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,65 @@
+// .9 Linking a JavaScript File
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10)
+
+//  .10 Values and Variables
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda ";
+// console.log(firstName);
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// console.log(myFirstJob )
+
+// Be descriptive with your variable names
+// Use camelCase
+// Uppercase all letters if the name is a true constant like PI = 3.14
+
+// .11 Practice Assessments
+// Done in assignments.js
+
+// .12 Data Types
+//  let javascriptIsFun = false
+//  console.log(javascriptIsFun)
+
+// .13 let, const, var
+
+// let age = 30
+// age = 31
+// const birthYear = 1985;
+// const job;
+// var job = "programmer"
+// job = "teacher"
+
+// .14 Basic Operations
+
+const now = 2037;
+const ageAmana = now - 1992;
+const ageJonas = now - 2018;
+
+console.log(ageAmana, ageJonas); // can log multiple values with the comma
+console.log(ageAmana * 2, ageAmana / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3, so 2 * 2 * 2
+
+const firstName = "Amana";
+const lastName = "Ammishaddai";
+
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4
+x++; // x + 1
+x--; // x - 1
+console.log(x);
+
+// Comparison operators
+
+console.log(ageAmana > ageJonas)
+
+// .15 Operator Precedence
